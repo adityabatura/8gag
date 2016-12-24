449ceb0a3f72eef7f694ea4341e52a62ceabe67c
adityabatura <aditya.batura@gmail.com>
2016-12-24 17:13:48 +0800

    first commit for this project

---
+++ Meme.swift
@@ -0,0 +1,15 @@
+//
+//  Meme.swift
+//  8gag
+//
+//  Created by Aditya Batura on 7/10/16.
+//  Copyright © 2016 Aditya Batura. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Meme {
+    var title:String = "default title"
+    var image:UIImage! = UIImage(named:"")
+}
